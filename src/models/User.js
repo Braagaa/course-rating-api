16af4a6c5bbe337734b9292246766e420f686cfd
Michael Braga <michael_braga@outlook.com>
2019-05-14 02:37:17 -0400

    email validation for User complete

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,18 @@
 const {Schema, model} = require('mongoose');
+const {validate: validEmail} = require('isemail');
 
 const UserSchema = new Schema({
     fullName: {type: String, required: true, trim: true},
-    emailAddress: {type: String, required: true, trim: true, unique: true},
+    emailAddress: {
+        type: String, 
+        required: true, 
+        trim: true, 
+        unique: true,
+        validate: {
+            validator: validEmail, 
+            message: '{VALUE} is not a valid email.'
+        }
+    },
     password: {type: String, required: true}
 });
 
