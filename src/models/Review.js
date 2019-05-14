47afee5366046647d1507cf52d73fc730ed6829a
Michael Braga <michael_braga@outlook.com>
2019-05-14 16:17:50 -0400

    Review validation may be complete. Testing later will ensure this.

--- src/models/Review.js
+++ src/models/Review.js
@@ -2,9 +2,17 @@ const {Schema, model} = require('mongoose');
 
 const ReviewSchema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: 'User'},
+    course: {type: Schema.Types.ObjectId, ref: 'Course'},
     postedOn: {type: Date, default: Date.now},
     rating: {type: Number, required: true, min: 1, max: 5},
     review: String
+});
+ReviewSchema.pre('save', function(next) {
+    const {course, user} = this;
+    if (course.user._id === user._id) {
+        throw new Error('You cannot review your own course.');
+    }
+    next();
 });
 
 const Review = model('Review', ReviewSchema);
